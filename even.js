aa8efeb265cfc31857436db79bcb6bf449a8d651
Shariare Emon <Shahriareemon2580@gmail.com>
2026-02-04 16:06:06 +0600

    add even

--- even.js
+++ even.js
@@ -1,10 +1,20 @@
-let num = 1;
-while(num <= 10) {
-    console.log(num);
+// let num = 1;
+// while(num <= 10) {
+//     console.log(num);
 
-    if(num % 2 == 0) {
-        console.log('even number/ jor songkha' , num)
-    }
+//     if(num % 2 == 0) {
+//         console.log('even number/ jor songkha' , num)
+//     }
 
-    num++;
+//     num++;
+// }
+
+//even
+for (let i= 0; i <= 20; i=i+2) {
+    console.log(i);
+}
+ 
+//odd
+for(let i =1; i <= 20; i=i+2) {
+    console.log(i);
 }
---
+++ forLoop.js
@@ -0,0 +1,3 @@
+for (let num = 10; num <= 10; num++) {
+    console.log(num);
+}

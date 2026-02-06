f3a3525e22d62d294235d37f998b2b7afcbaf56a
Shariare Emon <Shahriareemon2580@gmail.com>
2026-02-06 16:30:56 +0600

    loop is done

---
+++ break.js
@@ -0,0 +1,22 @@
+for(let i =0; i <20; i++) {
+    console.log(i);
+    if(i >=10) {
+        break;
+    }
+}
+console.log('break up hoise naki')
+
+
+let n = 55;
+while(n >44) {
+    console.log(n)
+    if(n <51) {
+        break;
+    }
+    n--;
+}
+console.log('mainas')
+
+
+
+//Opore break system deya ache.... 
---
+++ retired-hurt.js
@@ -0,0 +1,14 @@
+let num = 0;
+while(num <=200) {
+    console.log(num);
+
+    if(num >= 100) {
+         
+        break;
+    }
+  
+     num++;
+}
+// 
+
+`string`
